54a5e1e029dad4c9e14bdb8efb86d4985774c585
Alujack <yeantouch12345@gmail.com>
2024-05-16 00:16:26 +0700

    feat(fix):next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output:"export",
-  images: {
-    unoptimized: true,
-  },
+  output:'export',
+  reactStrictMode: true,
+  experimental: { serverActions: true },
 };
+
+module.exports = nextConfig;
